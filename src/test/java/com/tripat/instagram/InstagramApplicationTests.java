82ecd1386b204fafebb8ad41f14be0df4c70ecc3
tripat1803 <tripatjot1455.be21@chitkara.edu.in>
2024-02-17 08:47:49 +0530

    Configured dependencies

---
+++ src/test/java/com/tripat/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripat.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
