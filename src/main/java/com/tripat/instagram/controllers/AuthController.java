a6135c87b589ca1aec1592a69040a0afdeb139c0
tripat1803 <tripatjot1455.be21@chitkara.edu.in>
2024-02-20 20:38:45 +0530

    storing tokens in cookies

--- src/main/java/com/tripat/instagram/controllers/AuthController.java
+++ src/main/java/com/tripat/instagram/controllers/AuthController.java
@@ -17,26 +17,31 @@ import com.tripat.instagram.utils.AuthenticationRequest;
 import com.tripat.instagram.utils.AuthenticationResponse;
 import com.tripat.instagram.utils.RegisterRequest;
 import com.tripat.instagram.utils.Role;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @Autowired UserRepository userRepository;
-    @Autowired JwtService jwtService;
-    @Autowired PasswordEncoder passwordEncoder;
-    @Autowired AuthenticationManager authenticationManager;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    JwtService jwtService;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @GetMapping("")
     public String testAuth() {
         return "Hello";
     }
-    
 
     @PostMapping("/register")
-    ResponseEntity<?> registerUser(@RequestBody RegisterRequest request){
+    ResponseEntity<?> registerUser(@RequestBody RegisterRequest request, HttpServletResponse response) {
         User user = new User();
         user.setFirstname(request.firstname);
         user.setLastname(request.lastname);
@@ -46,21 +51,24 @@ public class AuthController {
         user.setPassword(passwordEncoder.encode(request.password));
         user.setRole(Role.USER);
         User savedUser = this.userRepository.save(user);
-        AuthenticationResponse response = new AuthenticationResponse();
-        response.access_token = jwtService.getnerateAccessToken(savedUser);
-        response.refresh_token = jwtService.getnerateRefreshToken(savedUser);
-        return ResponseEntity.ok(response);
+        String access_token = jwtService.getnerateAccessToken(savedUser);
+        String refresh_token = jwtService.getnerateRefreshToken(savedUser);
+        AuthenticationResponse res = new AuthenticationResponse(access_token, refresh_token);
+        response.addCookie(res.getAccessCookie());
+        response.addCookie(res.getRefreshCookie());
+        return ResponseEntity.ok("");
     }
 
     @PostMapping("/login")
-    ResponseEntity<?> authenticateUser(@RequestBody AuthenticationRequest request){
+    ResponseEntity<?> authenticateUser(@RequestBody AuthenticationRequest request, HttpServletResponse response) {
         authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(request.email, request.password)
-        );
+                new UsernamePasswordAuthenticationToken(request.email, request.password));
         User user = this.userRepository.findFirstByEmail(request.email);
-        AuthenticationResponse response = new AuthenticationResponse();
-        response.access_token = jwtService.getnerateAccessToken(user);
-        response.refresh_token = jwtService.getnerateRefreshToken(user);
-        return ResponseEntity.ok(response);
+        String access_token = jwtService.getnerateAccessToken(user);
+        String refresh_token = jwtService.getnerateRefreshToken(user);
+        AuthenticationResponse res = new AuthenticationResponse(access_token, refresh_token);
+        response.addCookie(res.getAccessCookie());
+        response.addCookie(res.getRefreshCookie());
+        return ResponseEntity.ok("");
     }
 }
